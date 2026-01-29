b734cbaf69dd54eb3fdca29d8a97797af94367a2
HAIDERMOBILES <h.store0066@icloud.com>
2026-01-29 09:34:10 +0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,29 +1,29 @@
-const CACHE_NAME = 'haider-mobiles-v1';
-const assets = [
-  '/',
-  '/index.html',
+const CACHE_NAME = 'haider-mobiles-cache-v1';
+const assetsToCache = [
+  '/haidermobile/',
+  '/haidermobile/index.html',
   'https://i.ibb.co/SSNjCH9/Haider-Mobile.png'
 ];
 
-// Install Service Worker
-self.addEventListener('install', e => {
-  e.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      cache.addAll(assets);
+// Install Event
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(assetsToCache);
     })
   );
 });
 
-// Activate & Clean Old Cache
-self.addEventListener('activate', e => {
-  console.log('App Service Worker Active');
+// Activate Event
+self.addEventListener('activate', (event) => {
+  console.log('Haider Mobiles Service Worker Activated');
 });
 
-// Fetch events
-self.addEventListener('fetch', e => {
-  e.respondWith(
-    caches.match(e.request).then(res => {
-      return res || fetch(e.request);
+// Fetch Event
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
     })
   );
 });
